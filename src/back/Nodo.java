32609d3e10add0c068f9971f901e7f029620f2c7
Bonnielamb6 <edgartrabajos2205@gmail.com>
2023-03-01 22:50:36 -0600

    Agregue las variables de hijo izquierdo e hijo derecho

---
+++ src/back/Nodo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package back;
+
+/**
+ *
+ * @author PC
+ */
+public class Nodo {
+    Nodo hijoDerecho;
+    Nodo hijoIzquierdo;
+}
