acaa63b80c3ced173e0dc56f80547b71085db112
AlonsoSotelo <cocolisojorge99@gmail.com>
2023-03-07 22:21:29 -0600

    Archivo Faltante No Se Porque xd
    Se copia de tu rama boni

--- src/back/NodoArbol.java
+++ src/back/NodoArbol.java
@@ -11,5 +11,70 @@ package back;
 public class NodoArbol {
     NodoArbol hijoDerecho;
     NodoArbol hijoIzquierdo;
-    String personaje;
+    String texto;
+    int profundidad;
+    
+    public NodoArbol(){
+        this.texto = "";
+        this.profundidad=0;
+        this.hijoIzquierdo = null;
+        this.hijoDerecho = null;
+    }
+    
+    public NodoArbol(String texto){
+        this.texto = texto;
+        this.profundidad=0;
+        this.hijoIzquierdo = null;
+        this.hijoDerecho = null;
+    }
+    
+    public NodoArbol(String caracteristica,NodoArbol hijoDerecho){
+        this.texto = caracteristica;
+        this.profundidad=0;
+        this.hijoIzquierdo = null;
+        this.hijoDerecho = hijoDerecho;
+    }
+
+    public NodoArbol getHijoDerecho() {
+        return hijoDerecho;
+    }
+
+    public void setHijoDerecho(NodoArbol hijoDerecho) {
+        this.hijoDerecho = hijoDerecho;
+    }
+
+    public NodoArbol getHijoIzquierdo() {
+        return hijoIzquierdo;
+    }
+
+    public void setHijoIzquierdo(NodoArbol hijoIzquierdo) {
+        this.hijoIzquierdo = hijoIzquierdo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public int getProfundidad() {
+        return profundidad;
+    }
+
+    public void setProfundidad(int profundidad) {
+        this.profundidad = profundidad;
+    }
+    
+    public NodoArbol avanzarDerecha(NodoArbol temporal){
+        temporal = temporal.hijoDerecho;
+        return temporal;
+    }
+    
+    public NodoArbol avanzarIzquierda(NodoArbol temporal){
+        temporal = temporal.hijoIzquierdo;
+        return temporal;
+    }
+    
 }
